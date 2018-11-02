c90137596c0313b072a210b4a9599cedd289ef98
TransportForLondonOutreach <44368116+TransportForLondonOutreach@users.noreply.github.com>
2018-11-02 12:54:36 +0000

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -5,29 +5,29 @@ gulp.task('copy-pics',function(){
     return gulp.src([
         './pics/*.png',
     ]) 
-    .pipe(gulp.dest('./dist/pics'));
+    .pipe(gulp.dest('./gh-pages/pics'));
 });
 
 gulp.task('copy-workers',function(){
     return gulp.src([
         './workers/*.js',
     ]) 
-    .pipe(gulp.dest('./dist/'));
+    .pipe(gulp.dest('./gh-pages/'));
 });
 
 gulp.task("noNotFound", () => {
     return gulp.src([
-        './dist/index.html',
+        './gh-pages/index.html',
     ]) 
     .pipe(rename("404.html"))
-    .pipe(gulp.dest('./dist/'));
+    .pipe(gulp.dest('./gh-pages/'));
 })
 
 gulp.task("noJekyll", () => {
     const path = require("path");
     const fs = require("fs");
 
-    fs.writeFileSync(path.join("./dist", ".nojekyll"), "")
+    fs.writeFileSync(path.join("./gh-pages", ".nojekyll"), "")
 })
 
 gulp.task("default", ["copy-pics", "noNotFound", "noJekyll", "copy-workers"]);
